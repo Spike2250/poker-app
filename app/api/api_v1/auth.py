85bea83713596844ae74f70cdb3b0e9d2635c6f7
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-05-27 19:48:48 +0500

    include auth router from fastapi-users

--- app/api/api_v1/auth.py
+++ app/api/api_v1/auth.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
 
+from api.dependencies.authentication.backend import authentication_backend
+from api.fastapi_users import fastapi_users
+
 
 router = APIRouter(
     tags=["Auth"],
 )
-
+router.include_router(
+    router=fastapi_users.get_auth_router(authentication_backend),
+)
