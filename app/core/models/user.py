8b09506c13b8a8f95ba69835771aa74e3bd8a150
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-05-27 15:12:51 +0500

    create User model

---
+++ app/core/models/user.py
@@ -0,0 +1,16 @@
+from fastapi_users.db import (
+    SQLAlchemyBaseUserTableUUID,
+    # SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
+)
+
+from .base import Base
+# from .mixins.int_id_pk import IntIdPkMixin
+
+
+class User(Base, SQLAlchemyBaseUserTableUUID):
+    pass
+
+
+# class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int])
+#     pass
