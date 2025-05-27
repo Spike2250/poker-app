06f9455fd8f613497208231bc0c83c2c1f166cd8
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-05-27 17:02:14 +0500

    rewrite User model to Int Id Pk

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,16 +1,16 @@
 from fastapi_users.db import (
-    SQLAlchemyBaseUserTableUUID,
-    # SQLAlchemyBaseUserTable,
+    # SQLAlchemyBaseUserTableUUID,
+    SQLAlchemyBaseUserTable,
     SQLAlchemyUserDatabase,
 )
 
 from .base import Base
-# from .mixins.int_id_pk import IntIdPkMixin
+from .mixins.int_id_pk import IntIdPkMixin
 
 
-class User(Base, SQLAlchemyBaseUserTableUUID):
-    pass
-
-
-# class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int])
+# class User(Base, SQLAlchemyBaseUserTableUUID):
 #     pass
+
+
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+    pass
