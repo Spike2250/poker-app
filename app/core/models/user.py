3d0bfe791a30970095994e67b2ec59eb3cc8d928
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-05-27 17:20:03 +0500

    add method for user database

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users.db import (
     # SQLAlchemyBaseUserTableUUID,
     SQLAlchemyBaseUserTable,
@@ -7,10 +9,15 @@ from fastapi_users.db import (
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 # class User(Base, SQLAlchemyBaseUserTableUUID):
 #     pass
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
-    pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
