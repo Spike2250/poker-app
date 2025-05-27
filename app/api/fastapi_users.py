72a832f90231f950d73d0b4ed7adc878b9209874
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-05-27 19:48:08 +0500

    move fastapi_users.py to api

--- app/api/fastapi_users.py
+++ app/api/fastapi_users.py
@@ -5,8 +5,8 @@ from fastapi_users import FastAPIUsers
 from core.models import User
 # from core.types.user_id import UserIdType
 
-from .user_manager import get_user_manager
-from .backend import authentication_backend
+from .dependencies.authentication.user_manager import get_user_manager
+from .dependencies.authentication.backend import authentication_backend
 
 
 # fastapi_users = FastAPIUsers[User, UserIdType](
