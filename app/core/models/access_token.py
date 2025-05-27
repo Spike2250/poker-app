df38911971363d4074ba882b13a00e657ecaf0b0
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-05-27 18:04:47 +0500

    Change user_id to UUID

--- app/core/models/access_token.py
+++ app/core/models/access_token.py
@@ -2,27 +2,40 @@ from typing import TYPE_CHECKING
 
 from fastapi_users_db_sqlalchemy.access_token import (
     SQLAlchemyAccessTokenDatabase,
-    SQLAlchemyBaseAccessTokenTable,
+    SQLAlchemyBaseAccessTokenTableUUID,
+    # SQLAlchemyBaseAccessTokenTable,
 )
-from sqlalchemy import(
-    Integer,
+from fastapi_users_db_sqlalchemy.generics import GUID
+from sqlalchemy import (
+    # Integer,
     ForeignKey,
 )
-from sqlalchemy.orm import(
+from sqlalchemy.orm import (
     Mapped,
     mapped_column,
 )
 
-from core.types.user_id import UserIdType
+# from core.types.user_id import UserIdType
 from .base import Base
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType]):
-    user_id: Mapped[UserIdType] = mapped_column(
-        Integer, ForeignKey("users.id", ondelete="cascade"),
+# class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType]):
+#     user_id: Mapped[UserIdType] = mapped_column(
+#         Integer, ForeignKey("users.id", ondelete="cascade"),
+#         nullable=False,
+#     )
+#
+#     @classmethod
+#     def get_db(cls, session: "AsyncSession"):
+#         return SQLAlchemyAccessTokenDatabase(session, cls)
+
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTableUUID):
+    user_id: Mapped[GUID] = mapped_column(
+        GUID, ForeignKey("users.id", ondelete="cascade"),
         nullable=False,
     )
 
