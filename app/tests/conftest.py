5df0690170db18f41ce0693752ccd42aef5079c6
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-06-29 23:15:49 +0500

    add pytest main fixtures

---
+++ app/tests/conftest.py
@@ -0,0 +1,63 @@
+import os
+import pytest
+from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker,
+    AsyncEngine,
+)
+from core.config import settings
+from core.models import (
+    Base,
+    db_helper,
+)
+from main import app
+
+
+os.environ['TESTING'] = "True"
+
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture(scope="function")
+async def engine():
+    engine: AsyncEngine = create_async_engine(settings.db.test_url)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield engine
+    await engine.dispose()
+
+
+@pytest.fixture
+async def db_session(engine: AsyncEngine):
+    session_factory = async_sessionmaker(
+        engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+    )
+    async with session_factory() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+
+@pytest.fixture
+async def async_client(db_session):
+    app.dependency_overrides[db_helper.session_getter] = lambda: db_session
+
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test"
+    ) as client:
+        yield client
+
+    app.dependency_overrides = {}
